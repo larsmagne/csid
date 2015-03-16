17aff97984d1f7cffd325c7ebad86c54aa81b3f0
Lars Magne Ingebrigtsen <larsi@gnus.org>
2015-03-16 13:44:17 +0100

    Implement a HTTP fetching version with a timeout

--- csid.el
+++ csid.el
@@ -136,6 +136,7 @@
     (loop for source in csid-sources
 	  for (name url function) = source
 	  for function = (intern (format "csid-parse-%s" function) obarray)
+	  do (message "%s" name)
 	  when (or (not type)
 		   (string= type name))
 	  append (let ((results
@@ -175,8 +176,82 @@
     (append elem (list (or found (incf csid-sequence))
 		       (format-time-string "%FT%T")))))
 
+(defun csid-retrieve-synchronously (url &optional silent inhibit-cookies)
+  "Retrieve URL synchronously.
+Return the buffer containing the data, or nil if there are no data
+associated with it (the case for dired, info, or mailto URLs that need
+no further processing).  URL is either a string or a parsed URL."
+  (url-do-setup)
+
+  (let ((retrieval-done nil)
+	(start-time (current-time))
+        (asynch-buffer nil))
+    (setq asynch-buffer
+	  (url-retrieve url (lambda (&rest ignored)
+			      (url-debug 'retrieval "Synchronous fetching done (%S)" (current-buffer))
+			      (setq retrieval-done t
+				    asynch-buffer (current-buffer)))
+			nil silent inhibit-cookies))
+    (if (null asynch-buffer)
+        ;; We do not need to do anything, it was a mailto or something
+        ;; similar that takes processing completely outside of the URL
+        ;; package.
+        nil
+      (let ((proc (get-buffer-process asynch-buffer)))
+	;; If the access method was synchronous, `retrieval-done' should
+	;; hopefully already be set to t.  If it is nil, and `proc' is also
+	;; nil, it implies that the async process is not running in
+	;; asynch-buffer.  This happens e.g. for FTP files.  In such a case
+	;; url-file.el should probably set something like a `url-process'
+	;; buffer-local variable so we can find the exact process that we
+	;; should be waiting for.  In the mean time, we'll just wait for any
+	;; process output.
+	(while (and (not retrieval-done)
+		    (< (float-time (time-subtract (current-time) start-time))
+		       5))
+	  (url-debug 'retrieval
+		     "Spinning in url-retrieve-synchronously: %S (%S)"
+		     retrieval-done asynch-buffer)
+          (if (buffer-local-value 'url-redirect-buffer asynch-buffer)
+              (setq proc (get-buffer-process
+                          (setq asynch-buffer
+                                (buffer-local-value 'url-redirect-buffer
+                                                    asynch-buffer))))
+            (if (and proc (memq (process-status proc)
+                                '(closed exit signal failed))
+                     ;; Make sure another process hasn't been started.
+                     (eq proc (or (get-buffer-process asynch-buffer) proc)))
+                ;; FIXME: It's not clear whether url-retrieve's callback is
+                ;; guaranteed to be called or not.  It seems that url-http
+                ;; decides sometimes consciously not to call it, so it's not
+                ;; clear that it's a bug, but even then we need to decide how
+                ;; url-http can then warn us that the download has completed.
+                ;; In the mean time, we use this here workaround.
+		;; XXX: The callback must always be called.  Any
+		;; exception is a bug that should be fixed, not worked
+		;; around.
+		(progn ;; Call delete-process so we run any sentinel now.
+		  (delete-process proc)
+		  (setq retrieval-done t)))
+            ;; We used to use `sit-for' here, but in some cases it wouldn't
+            ;; work because apparently pending keyboard input would always
+            ;; interrupt it before it got a chance to handle process input.
+            ;; `sleep-for' was tried but it lead to other forms of
+            ;; hanging.  --Stef
+            (unless (or (with-local-quit
+			  (accept-process-output proc 1))
+			(null proc))
+              ;; accept-process-output returned nil, maybe because the process
+              ;; exited (and may have been replaced with another).  If we got
+	      ;; a quit, just stop.
+	      (when quit-flag
+		(delete-process proc))
+              (setq proc (and (not quit-flag)
+			      (get-buffer-process asynch-buffer)))))))
+      asynch-buffer)))
+
 (defun csid-parse-source (url function data-type)
-  (with-current-buffer (url-retrieve-synchronously url)
+  (with-current-buffer (csid-retrieve-synchronously url t t)
     (goto-char (point-min))
     (when (search-forward "\n\n" nil t)
       (let* ((headers (eww-parse-headers))
@@ -625,7 +700,7 @@
 	return (csid-parse-vcalendar (concat "http:" (match-string 1 href)))))
 
 (defun csid-parse-vcalendar (url)
-  (with-current-buffer (url-retrieve-synchronously url)
+  (with-current-buffer (csid-retrieve-synchronously url t t)
     (goto-char (point-min))
     (while (re-search-forward "\r" nil t)
       (replace-match "" t t))

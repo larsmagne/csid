cb5b919410cd4d4012ce40918051e6eeea930151
Lars Magne Ingebrigtsen <larsi@gnus.org>
2015-09-09 14:52:03 +0200

    Copy over changes from app-csid

--- csid.js
+++ csid.js
@@ -630,6 +630,8 @@ function removeScrollActions() {
 }
 
 function setHardWidths() {
+  if (device.platform != "iOS")
+    return;
   var width = $(window).width() - 100;
   $("tr").each(function(key, node) {
     $(node).children("td").first().css({

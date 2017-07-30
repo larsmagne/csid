60b8e43a0133cb4bf1db48f3c0ce3a983422a89b
Lars Ingebrigtsen <larsi@gnus.org>
2017-07-30 13:51:08 +0200

    Remove success string now that it's interactive

--- csid.js
+++ csid.js
@@ -1022,8 +1022,6 @@ function exportEvent(id) {
   var endDate = new Date(date[0], date[1] - 1, date[2], 20, 00, 0, 0, 0);
 
   var success = function(message) {
-    colorbox("<a href='#' id='csid-close'>Added event " + title +
-	     " to calendar</a>");
   };
   var error = function(message) {
     alert("Unable to export event: " + message);

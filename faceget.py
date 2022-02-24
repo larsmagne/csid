79bf8c223d3cf53c3fa2217428a7973055f7a077
Lars Ingebrigtsen <larsi@gnus.org>
2022-02-24 17:32:57 +0100

    This apparently doesn't work headless any more

--- faceget.py
+++ faceget.py
@@ -32,7 +32,7 @@ chrome_options = webdriver.ChromeOptions()
 prefs = {"profile.default_content_setting_values.notifications" : 2}
 chrome_options.add_experimental_option("prefs", prefs)
 chrome_options.add_argument("--disable-notifications")
-chrome_options.add_argument("--headless")
+#chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage");
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_experimental_option('prefs', {'intl.accept_languages': 'no'})

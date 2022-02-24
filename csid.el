9bebef04a19a131745d5da4db5c6796a2b7b4cad
Lars Ingebrigtsen <larsi@gnus.org>
2022-02-24 17:50:29 +0100

    Remove Henie Onstad because they change every other week

--- csid.el
+++ csid.el
@@ -105,7 +105,7 @@
     ("Krøsset" "https://www.facebook.com/krxsset/events?ref=page_internal" facebook (59.921141 10.751589))
     ;;("Cappelens forslag" "https://www.facebook.com/CappelensForslag/events" facebook (59.915077 10.753527))
     ;;("Barrikaden" "http://vestbredden.net/barrikaden/" barrikaden :date)
-    ("Henie Onstad" "http://hok.no/kalender" henie-onstad :date (59.888617 10.553501) :nobound)
+    ;;("Henie Onstad" "http://hok.no/kalender" henie-onstad :date (59.888617 10.553501) :nobound)
     ("Khartoum" "https://nb-no.facebook.com/pg/khartoumcontemporary/events/?ref=page_internal" facebook (59.917118 10.750163))
     ("Salt" "https://www.facebook.com/pg/SaltArtMusic/events/?ref=page_internal" facebook (59.90704963300261 10.746273380187537))
     ("Røverstaden" "https://www.facebook.com/pg/roverstaden/events/?ref=page_internal" facebook (59.913562 10.729115))

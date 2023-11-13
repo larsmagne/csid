f6014b55f0e6cd8f2b95b89e473017bbbf1d2e85
Lars Ingebrigtsen <larsi@gnus.org>
2023-11-13 23:57:47 +0100

    Oslo Bluesklubb is on Facebook

--- csid.el
+++ csid.el
@@ -120,7 +120,7 @@
     ("Rock In" "https://www.facebook.com/pg/rockinoslo/events/?ref=page_internal" facebook (59.913002 10.761144))
     ("Brød & Sirkus" "https://www.facebook.com/brodogsirkus/events/" facebook (59.91311858805727 10.736465987756189))
     ;;("Oslo Jazzfestival" "https://oslojazz.no/program/" oslo-jazzfestival)
-    ("Oslo Bluesklubb" "https://www.osloblues.com/konsertarkiv/" oslo-bluesklubb)
+    ("Oslo Bluesklubb" "https://www.facebook.com/oslobluesklubb/events?locale=nb_NO" facebook)  ;; Non-Facebook available
     ("Kampen Kaffe & Bar" "https://www.facebook.com/kampenkaffebar/events" facebook (59.911972107696684 10.781134433609944))
     ("MS Bjørvika" "https://www.facebook.com/msbjorvika/events/" facebook (59.905780304973966 10.75342868368582))
     ("Godthåb" "https://www.facebook.com/godthab/events/?ref=page_internal" facebook (59.91293095965448 10.761361703327038))

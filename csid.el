e67d289200160b015b52cbb200097124b6722eb4
Lars Ingebrigtsen <larsi@gnus.org>
2023-11-13 23:56:10 +0100

    No 53 is gone

--- csid.el
+++ csid.el
@@ -105,7 +105,6 @@
     ("Khartoum" "https://nb-no.facebook.com/pg/khartoumcontemporary/events/?ref=page_internal" facebook (59.917118 10.750163))
     ("Salt" "https://www.facebook.com/pg/SaltArtMusic/events/?ref=page_internal" facebook (59.90704963300261 10.746273380187537))
     ("Røverstaden" "https://www.facebook.com/pg/roverstaden/events/?ref=page_internal" facebook (59.913562 10.729115))
-    ("No 53" "https://www.facebook.com/pg/No-53-139168752824276/events/?ref=page_internal" facebook (59.908699 10.767789))
     ;;("LilleKampen" "https://www.facebook.com/pg/lillekampen/events/?ref=page_internal" facebook (59.913884 10.781574))
     ("Vaterland" "https://www.facebook.com/pg/vaterlandoslo/events/?ref=page_internal" facebook (59.913885 10.756072))
     ("Rommen scene" "https://www.rommenscene.no/program/" rommen (59.967347 10.914572) :nobound)
